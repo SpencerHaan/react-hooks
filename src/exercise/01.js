23f9a3ebb0dce49f0bc98bffc915cd5d133b40df
Spencer Haan <spencer.haan+github@shopify.com>
2021-11-28 19:25:49 -0500

    Exercise 1 extra credit solution

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,8 +3,8 @@
 
 import * as React from 'react'
 
-function Greeting() {
-  const [name, setName] = React.useState('')
+function Greeting({initialName = ''}) {
+  const [name, setName] = React.useState(initialName)
 
   function handleChange(event) {
     const {value} = event.target
@@ -15,7 +15,7 @@ function Greeting() {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -23,7 +23,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName='World' />
 }
 
 export default App
